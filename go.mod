5979cb6e40ace07a0fcd7bf46dc0c5f93b9c3b87
Taha Ahmadi <tahaahmadimail@protonmail.com>
2022-12-24 16:41:58 +0330

    add hw

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taha-ahmadi/practical-go
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//s := "a😊"
+
+	fmt.Println(len([]rune("世界")))
+
+}
+
+type Taha struct {
+	ali []any
+}
